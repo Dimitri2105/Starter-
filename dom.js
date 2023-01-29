66f203881b60fe883d7595b83fe6163a25acfe1e
Dimitri2105 <karanthakur577@gmail.com>
2023-01-29 13:32:16 +0530

    GetElementsbyClassname

--- dom.js
+++ dom.js
@@ -32,9 +32,11 @@
 let items = document.getElementsByClassName("list-group-item");
 console.log(items);
 console.log(items[2]);
+items[1].style.fontWigth = "bold";
 items[2].textContent = "hey there";
 items[2].style.fontWeight = "Bold";
 items[2].style.backgroundColor = "green";
+
 
 for (let i = 0; i<items.length;i++){
 
