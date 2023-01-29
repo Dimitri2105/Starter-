42aade497340e6b111c47d9a974d64534b06b76d
Dimitri2105 <karanthakur577@gmail.com>
2023-01-29 12:17:48 +0530

    Dom manipulations

---
+++ dom.js
@@ -0,0 +1,30 @@
+// // console.dir(document);
+
+// // console.dir(document.head);
+// // console.dir(document.body);
+// // console.dir(document.all);
+// // console.dir(document.all[10]);
+// // console.dir(document.forms);
+// // console.dir(document.links);
+// console.dir(document.images);
+
+
+// console.log(document.getElementById('header-title'));
+
+let header_title = document.getElementById('header-title');
+
+// console.log(header_title);
+
+// header_title.textContent = "hello";
+// header_title.innerText = "Wow";
+
+// header_title.innerHTML = "<h1> hello there </h1>" 
+
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = "solid 3px black";
+
+// document.querySelector(".title").style.fontWeight = "bold";
+
+// document.querySelector(".title").style.color = "Green" ; 
+
+
