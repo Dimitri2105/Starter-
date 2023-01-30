51061c4e556779c51dab3cf5446936ca7c681a92
Dimitri2105 <karanthakur577@gmail.com>
2023-01-30 20:20:21 +0530

    Scale the app to more users deliverable

--- JavaScript.js
+++ JavaScript.js
@@ -25,41 +25,77 @@
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phonenumber');
+const item = document.getElementById('users');
+
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
-myForm.addEventListener('submit',onSubmit);
+// myForm.addEventListener('submit',onSubmit);
+myForm.addEventListener('submit',addItem);
 
-function onSubmit(e){
+// function onSubmit(e){
 
-    e.preventDefault();
+//     e.preventDefault();
     
-    // if(nameInput.value === '' || emailInput.value ===''){
-    //     msg.innerHTML = 'Please enter all fields'
-    // }
-    // else(
+//     // if(nameInput.value === '' || emailInput.value ===''){
+//     //     msg.innerHTML = 'Please enter all fields'
+//     // }
+//     // else(
 
-        // localStorage.setItem('Username',nameInput.value)
-        // localStorage.setItem('email',emailInput.value)
+//         // localStorage.setItem('Username',nameInput.value)
+//         // localStorage.setItem('email',emailInput.value)
 
-        // STORING USERDETAILS AS OBJECTS IN LOCAL STORAGE
+//         // STORING USERDETAILS AS OBJECTS IN LOCAL STORAGE
 
-        nameAsString = JSON.stringify(nameInput.value)
-        emailAsString = JSON.stringify(emailInput.value)
+//         nameAsString = JSON.stringify(nameInput.value)
+//         emailAsString = JSON.stringify(emailInput.value)
+//         phoneAsString = JSON.stringify(phoneInput.value)
 
-        localStorage.setItem('Username',nameAsString)
-        localStorage.setItem('email',emailAsString)
+//         localStorage.setItem('Username',nameAsString)
+//         localStorage.setItem('email',emailAsString)
+//         localStorage.setItem('phoneNumber',phoneAsString)
 
         
 
 
 
 
-        // console.log(nameInput.value,   emailInput.value)
+
+        
+
+
+
+
+//         // console.log(nameInput.value,   emailInput.value)
         
         
-    // )
-}
+//     // )
+// }
 
 
 // -----------------------------------------------
+
+function addItem(e){
+    e.preventDefault();
+
+    // Grabbing input values
+    let user = document.getElementById('name').value;
+    let emailID = document.getElementById('email').value;
+    let phonenumber = document.getElementById('phonenumber').value;
+    
+    //  creating new li element
+    let li = document.createElement('li');
+
+    // adding class
+    // li.className='items';
+
+    // adding the text node with input to li
+    li.appendChild(document.createTextNode(user))
+    li.appendChild(document.createTextNode(emailID))
+    li.appendChild(document.createTextNode(phonenumber))
+
+
+    item.appendChild(li);
+
+}
